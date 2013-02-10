bf55254d4148ab8bfe8b38b9ec73e0e710e0ad0a
Maika <maikai.life@gmail.com>
2013-02-09 17:17:57 -0800

    Fanal before public

--- wp-content/themes/pinboard/header.php
+++ wp-content/themes/pinboard/header.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?php wp_title(); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/fork_knife-favi.png" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <?php wp_head(); ?>
 </head>
