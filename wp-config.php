1e387f151026e7b98492fa469dbc2723903edae4
Casey Wilms <casey@bikoo.jp>
2014-04-12 12:33:44 -0700

    Delete Facebook Plugin
    We don’t need no stinkin facebook

--- wp-config.php
+++ wp-config.php
@@ -34,7 +34,7 @@ if (isset($_SERVER['PLATFORM']) && $_SERVER['PLATFORM'] == 'PAGODABOX') {
     define ('DB_HOST', $_SERVER['DB1_HOST'] . ':' . $_SERVER['DB1_PORT']);
 }
 else {
-    define('DB_NAME', 'wordpress');
+    define('DB_NAME', 'wordpress-sfbb');
     define('DB_USER', 'root');
     define('DB_PASSWORD', 'root');
     define('DB_HOST', 'localhost');
